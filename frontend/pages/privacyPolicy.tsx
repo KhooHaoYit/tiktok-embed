01b5f5ff538d4ec9930056d45af3f6126dded73c
Khoo Hao Yit <khoohaoyit16@gmail.com>
2023-07-28 04:29:29 +0000

    add tos and privacy policy page

---
+++ frontend/pages/privacyPolicy.tsx
@@ -0,0 +1,21 @@
+export default function Page() {
+  // Exclusively used for error tracking/performance evaluation
+  // Retained for 90 days (Default sentry configuration)
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <h2>Information Collection</h2>
+      We may collect certain information when you interact with our website or services. This data includes but is not limited to your IP address, device information, browser type, and interactions with our platform.
+      <h2>Data Usage</h2>
+      The information collected will be used exclusively for error tracking and performance evaluation purposes. This helps us identify and rectify any technical issues, improve our services, and enhance user experience.
+      <h2>Data Retention</h2>
+      We will retain the collected data for a period of 90 days from the date of collection. After this period, all information will be securely deleted or anonymized, ensuring that no personally identifiable data is retained beyond the specified time frame.
+      <h2>Data Sharing</h2>
+      Your information will not be shared with any third parties unless required by law or for legal purposes.
+      <h2>Data Security</h2>
+      We maintain strict security measures to protect your data from unauthorized access, alteration, or disclosure.
+      <h2>Consent</h2>
+      By using our website or services, you consent to the collection, use, and retention of your information as described in this Privacy Policy.
+    </div>
+  );
+}
---
+++ frontend/pages/termsOfService.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Terms of Service</h1>
+      Please read these terms of service ("terms", "terms of service") carefully before using our bot (the "service").
+      <h2>Conditions of Use</h2>
+      Usage of this bot is subject to the terms below. By using the bot you agree to these terms.
+      <h2>Privacy Policy</h2>
+      Before you continue using our website we advise you to read our
+      {' '}<a href="/privacyPolicy">privacy policy</a>{' '}
+      regarding our user data collection.
+    </div>
+  );
+}
