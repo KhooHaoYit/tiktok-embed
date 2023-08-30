0c14162814703c1f6f08c516036003e4b1f757b8
Khoo Hao Yit <khoohaoyit16@gmail.com>
2023-08-30 14:38:34 +0000

    remove internal frontend url

--- discord-bot/src/env.ts
+++ discord-bot/src/env.ts
@@ -11,7 +11,6 @@ export const env = createEnv({
         .split(',')
         .filter(token => token)),
     FRONTEND_URL: z.string(),
-    INTERNAL_FRONTEND_URL: z.string(),
     SUGGESTION_CHANNEL_ID: z.string().optional(),
     SUPPORT_SERVER_INVITE_LINK: z.string().optional(),
     ENABLE_REPL: z.coerce.boolean().default(false),
