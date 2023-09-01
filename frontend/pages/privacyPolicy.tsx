0dbe27316032ebd1a64932001254ffbe88e1feb4
Khoo Hao Yit <khoohaoyit16@gmail.com>
2023-09-01 08:15:08 +0000

    update privacyPolicy

--- frontend/pages/privacyPolicy.tsx
+++ frontend/pages/privacyPolicy.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
-  // Exclusively used for error tracking/performance evaluation
+  // Exclusively used for error tracking/performance evaluation/service configuration
   // Retained for 90 days (Default sentry configuration)
   return (
     <div>
@@ -7,7 +7,7 @@ export default function Page() {
       <h2>Information Collection</h2>
       We may collect certain information when you interact with our website or services. This data includes but is not limited to your IP address, device information, browser type, and interactions with our platform.
       <h2>Data Usage</h2>
-      The information collected will be used exclusively for error tracking and performance evaluation purposes. This helps us identify and rectify any technical issues, improve our services, and enhance user experience.
+      The information collected will be used exclusively for error tracking, performance evaluation, and service configuration purposes. This helps us identify and rectify any technical issues, improve our services, and enhance user experience.
       <h2>Data Retention</h2>
       We will retain the collected data for a period of 90 days from the date of collection. After this period, all information will be securely deleted or anonymized, ensuring that no personally identifiable data is retained beyond the specified time frame.
       <h2>Data Sharing</h2>
